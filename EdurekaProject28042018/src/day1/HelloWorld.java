0fad3c6abeddb0a47aadedaf05c4f58e821cc225
Saurabh Dhingra <saurabh.d2106@gmail.com>
2018-05-19 20:45:14 +0530

    Updating hello world from github

--- EdurekaProject28042018/src/day1/HelloWorld.java
+++ EdurekaProject28042018/src/day1/HelloWorld.java
@@ -6,9 +6,7 @@ public class HelloWorld {
 		
 		System.out.println("Hello World ");
 		
-		System.out.println("Hello world again");
 		
-		System.out.println("Hello world again and again!!");
 	}
 
 }
